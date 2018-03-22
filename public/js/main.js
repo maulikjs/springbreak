b13cae51f023edc16e61291ff73bdea9e2f48e07
Sharvil <sparekh@whiskerlabs.com>
2018-03-21 21:22:53 -0400

    Added label support

--- public/js/main.js
+++ public/js/main.js
@@ -10,10 +10,10 @@ $('#calendar').fullCalendar({
 
 $.get("/schools", function(data, status){
       $('.ui.search').search({source: data, onSelect: function (result,response) {
-            
+            var html = '<div class="ui label large red">' + result.title + '<i class="delete icon" onclick="deleteLabel(this)"></i></div>';
+            $('.selectedSchools').append(html)
       }});
 });
-
 
 var deleteLabel = function(e) {
   $(e).parent().remove();
